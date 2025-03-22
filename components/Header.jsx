363b6e9cf0f8caf5cf8ba71c76cb0a99f86a7ad0
hamwent rawat <hamwentrawat@hamwents-MacBook-Air.local>
2025-03-22 19:59:28 +0800

    country app final dev

---
+++ components/Header.jsx
@@ -0,0 +1,26 @@
+import { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+
+export default function Header() {
+  const [isDark, setIsDark] = useContext(ThemeContext);
+
+  return (
+    <header className={`header-container ${isDark ? "dark" : ""}`}>
+      <div className="header-content">
+        <h2 className="title">
+          <a href="/">Where in the world?</a>
+        </h2>
+        <p
+          className="theme-changer"
+          onClick={() => {
+            setIsDark(!isDark);
+            localStorage.setItem("isDarkMode", !isDark);
+          }}
+        >
+          <i className={`fa-solid fa-${isDark ? "moon" : "sun"}`} />
+          &nbsp;&nbsp;{isDark ? "Dark" : "Light"} Mode
+        </p>
+      </div>
+    </header>
+  );
+}
---
+++ components/Loader.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Loader = () => {
+  return <h2>Country Details Loading...</h2>;
+};
+
+export default Loader;
